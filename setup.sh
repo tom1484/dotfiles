bcebb1d35971c74b6d6168540039e840e6b39329
tom1484 <tomchen2003611@gmail.com>
2023-07-28 00:36:10 +0800

    udpate

--- setup.sh
+++ setup.sh
@@ -45,6 +45,11 @@ sudo ln -s /usr/bin/batcat /usr/bin/bat
 # broot
 cargo install --locked broot
 broot
+# laygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
 
 ### Configuration
 cd ./fish && sh ./setup.sh && cd ../
