8c2e78bb3d55923082cac43a2793be6aa58baa9c
tom1484 <tomchen2003611@gmail.com>
2024-12-21 19:21:46 +0800

    update

--- mac/fish/config/config/plugins/fifc.fish
+++ mac/fish/config/config/plugins/fifc.fish
@@ -1,1 +1,19 @@
 set -U fifc_exa_opts --icons --long --no-user --no-time
+
+# function trim_trailing_space
+#     # Get the last command line
+#     set -l last_command (commandline -p)
+#     # Trim trailing spaces
+#     set last_command (string trim $last_command)
+#     # Set the command line back without trailing spaces
+#     commandline -r $last_command
+# end
+
+# Hook into the selection process
+# function fifc_select
+#     # Call your original selection command here
+#     fifc
+#     # Trim trailing space after selection
+#     trim_trailing_space
+# end
+
--- mac/fish/config/config/plugins/fzf.fish
+++ mac/fish/config/config/plugins/fzf.fish
@@ -5,4 +5,4 @@ fzf_configure_bindings --history=\cH
 fzf_configure_bindings --processes=\cP
 fzf_configure_bindings --variables=\cV
 
-set -gx FZF_DEFAULT_OPTS --bind=tab:down,shift-tab:up
+set -gx FZF_DEFAULT_OPTS --bind=tab:down,shift-tab:up --layout=reverse --height=50%
