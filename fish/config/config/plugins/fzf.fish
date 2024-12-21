eb4334321e834cf92acce56cedab8167a6c7875b
tom1484 <tomchen2003611@gmail.com>
2024-12-21 19:50:17 +0800

    update

--- fish/config/config/plugins/fzf.fish
+++ fish/config/config/plugins/fzf.fish
@@ -1,8 +1,8 @@
-fzf_configure_bindings --directory=\cF
-fzf_configure_bindings --git_log=\cL
-fzf_configure_bindings --git_status=\cS
-fzf_configure_bindings --history=\cH
-fzf_configure_bindings --processes=\cP
-fzf_configure_bindings --variables=\cV
+fzf_configure_bindings --directory=\cf
+fzf_configure_bindings --git_log=\cl
+fzf_configure_bindings --git_status=\cs
+fzf_configure_bindings --history=\ch
+fzf_configure_bindings --processes=\cp
+fzf_configure_bindings --variables=\cv
 
 set -gx FZF_DEFAULT_OPTS --bind=tab:down,shift-tab:up --layout=reverse --height=50%
