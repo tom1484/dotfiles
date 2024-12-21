9a4e643087be0e631bb500bfaf889c9148460dc7
tom1484 <tomchen2003611@gmail.com>
2024-12-21 19:25:08 +0800

    update

--- mac/fish/setup.sh
+++ mac/fish/setup.sh
@@ -10,16 +10,14 @@ chsh -s $(which fish)
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 # starship
 cargo install starship
-# fzf.fish
-fisher install PatrickF1/fzf.fish
-# z
+
+fisher install jorgebucaran/fisher
+fisher install patrickf1/fzf.fish
 fisher install jethrokuan/z
-# sponge
-fisher install andreiborisov/sponge
-# auto-pair
 fisher install jorgebucaran/autopair.fish
-# nvm
 fisher install jorgebucaran/nvm.fish
+fisher install gazorby/fifc
+
 nvm install v18.14.2
 # mamba
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh -O tmp/mamba.sh
